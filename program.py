a5a5c22f64da0a6525b60e952edac9a5ceabdb4f
SCOTT STUDENT <scotbenj@sheridancollege.com>
2024-09-25 17:35:25 -0400

    added my first git!

---
+++ program.py
@@ -0,0 +1,9 @@
+# Assignment 1
+# PROG 10004
+# Rock paper scissors assignment.
+
+print("------- WELCOME TO ROCK PAPER SCISSORS -------")
+
+games = int(input("How many matches would you like to play?\n> "))
+computerWins = 0
+playerWins = 0
