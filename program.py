35b8e96fc4b6d10c95f770bfcd69dbe622fc5053
SCOTT STUDENT <scotbenj@sheridancollege.com>
2024-09-25 19:46:03 -0400

    added comments and refined code

--- program.py
+++ program.py
@@ -1,19 +1,25 @@
 # Assignment 1
 # PROG 10004
 # Rock paper scissors assignment.
+# This is not complex and doesn't require multiple files.
 
 from math import *
 from random import *
 
-
+# Displays the computer vs player standings so the player knows how close they are to winning/losing.
 def standings() :
     global computerWins
     global games
     global playerWins
+    global computerChoice
+    global playerChoice
 
+    print(f"Computer: {computerChoice} vs Player: {playerChoice}")
     print(f"The standings are...\nComputer: {computerWins}/{games}.\nPlayer: {playerWins}/{games}")
 
-def computerMath() :
+
+# Makes the computer pick randomly between rock paper and scissors so it can fight the user fairly.
+def randomRPS() :
     global computerChoice
     number = (randrange(1,3))
     if number == 1 :
@@ -23,53 +29,74 @@ def computerMath() :
     elif number == 3 :
         computerChoice = "S"
 
-
+# It runs this and doesn't update any info if the player and computer tie.
 def tie() :
     print("You and the computer tied.")
     standings()
 
+# Is run when the player wins.
 def win() :
     global playerWins
     playerWins = playerWins + 1
     print("You beat the computer!")
     standings()
-
+    
+# Is run when the player loses.
 def loss() :
     global computerWins
     computerWins = computerWins + 1
     print("You lost.")
     standings()
+    
 
 
-print("\n\n------- WELCOME TO ROCK PAPER SCISSORS -------")
+# Introduces the game to the player.
 
-games = int(input("How many matches would you like to play?\n> "))
+print("\n-------------- WELCOME TO ROCK PAPER SCISSORS --------------")
+print("Instructions:\nYou are playing rock paper and scissors against the worlds strongest super computer.\n If you don't beat it, It will take over the world!")
+games = int(input("\nHow many matches would you like to play?\n> "))
 computerWins = 0
 playerWins = 0
 computerChoice = 0
+playerChoice = 0
+
+# Main logic loop so that we can tell who wins/loses. 
+# Also tells us which score we need to add to.
 
 while playerWins < games and computerWins < games :
-    playerChoice = input("\nPlease select rock (R), paper (P) or scissors (S).\n> ")
-    playerChoice = playerChoice.upper()
-    computerMath()
+    playerChoice = input("\n\nPlease select rock (R), paper (P) or scissors (S).\n> ")
 
+    # Refines player and computer choices so they can be evaluated.
+    playerChoice = playerChoice.upper()
+    randomRPS()
+
+    # Quits application.
     if playerChoice == "quit" :
         break
+
+    # Logic for computer - rock
     if computerChoice == "R" :
+
         if playerChoice == "R" :
             tie()
         elif playerChoice == "P" :
             win()
         elif playerChoice == "S" :
             loss()
+
+    # Logic for computer - paper
     elif computerChoice == "P" :
+
         if playerChoice == "R" :
             loss()
         elif playerChoice == "P" :
             tie()
         elif playerChoice == "S" :
             win()
+
+    # Logic for computer - scissors
     elif computerChoice == "S" :
+
         if playerChoice == "R" :
             win()
         elif playerChoice == "P" :
@@ -77,9 +104,12 @@ while playerWins < games and computerWins < games :
         elif playerChoice == "S" :
             tie()
 
+
+# Concludes the game with a fun message!
+
 if playerWins == games :
-    print("You won!")
+    print("\n\n\nYou stopped the computer from taking over!\nCongratulations! (GOOD ENDING)")
 else :
-    print("You lost.")
+    print("\n\n\nYou let the computer take over... (BAD ENDING)")
 
 print("Thanks for playing!")
