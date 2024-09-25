3f7817d4fec8a74eb3acbbaab798f5f467e5cb00
SCOTT STUDENT <scotbenj@sheridancollege.com>
2024-09-25 18:47:03 -0400

    i hope these commits are working

--- program.py
+++ program.py
@@ -6,19 +6,33 @@ from math import *
 from random import *
 
 
-def computerChoice() :
-    print(randrange(1,3))
+def standings() :
+    print(f"The standings are...\nComputer: {computerWins}/{games}.\nPlayer: {playerWins}/{games}")
+
+def computerMath() :
+    global computerChoice
+    number = (randrange(1,3))
+    if number == 1 :
+        computerChoice = "R"
+    elif number == 2 :
+        computerChoice = "P"
+    elif number == 3 :
+        computerChoice = "S"
+
 
 def tie() :
     print("You and the computer tied.")
+    standings()
 
 def win() :
     playerWins = playerWins + 1
     print("You beat the computer!")
+    standings()
 
 def loss() :
     computerWins = computerWins + 1
     print("You lost.")
+    standings()
 
 
 print("------- WELCOME TO ROCK PAPER SCISSORS -------")
@@ -26,12 +40,20 @@ print("------- WELCOME TO ROCK PAPER SCISSORS -------")
 games = int(input("How many matches would you like to play?\n> "))
 computerWins = 0
 playerWins = 0
+computerChoice = 0
 
 while playerWins or computerWins < games :
     playerChoice = input("Please select rock (R), paper (P) or scissors (S).")
-    computerChoice()
+    computerMath()
 
     if computerChoice == "R" :
+        if playerChoice == "R" :
+            tie()
+        elif playerChoice == "P" :
+            win()
+        elif playerChoice == "S" :
+            loss()
+    elif computerChoice == "P" :
         if playerChoice == "R" :
             tie()
         elif playerChoice == "P" :
