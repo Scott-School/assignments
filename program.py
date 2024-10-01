1e1a76c615535e3984f02ecaef27b867efb3b273
SCOTT STUDENT <scotbenj@sheridancollege.com>
2024-10-01 11:16:03 -0400

    gave myself a headache and completed the game!

--- program.py
+++ program.py
@@ -1,13 +1,20 @@
 # Assignment 1
 # PROG 10004
 # Rock paper scissors assignment.
-# This is not complex and doesn't require multiple files.
-# TODO: Make the entire thing run on a loop that can be replayed at the end of the game.
+# Scott
 
 from math import *
 from random import *
 
 # Displays the computer vs player standings so the player knows how close they are to winning/losing.
+
+computerWins = 0
+playerWins = 0
+computerChoice = 0
+playerChoice = 0
+games = 0
+
+
 def standings() :
     global computerWins
     global games
@@ -20,22 +27,24 @@ def standings() :
     elif computerChoice == "P" :
         computerChoice = "Paper"
     elif computerChoice == "S" :
-        computerChoice == "Scissors"
+        computerChoice = "Scissors"
 
     if playerChoice == "R" :
         playerChoice = "Rock"
     elif playerChoice == "P" :
         playerChoice = "Paper"
     elif playerChoice == "S" :
-        playerChoice == "Scissors"
-    print(f"Computer: {computerChoice} vs Player: {playerChoice}")
+        playerChoice = "Scissors"
+    print(f"Player: {playerChoice} | Computer: {computerChoice}")
     print(f"The standings are...\nComputer: {computerWins}/{games}.\nPlayer: {playerWins}/{games}")
 
 
 # Makes the computer pick randomly between rock paper and scissors so it can fight the user fairly.
+# I found randrange in the random python library!
 def randomRPS() :
     global computerChoice
-    number = (randrange(1,3))
+
+    number = (randrange(1,4))
     if number == 1 :
         computerChoice = "R"
     elif number == 2 :
@@ -62,37 +71,60 @@ def loss() :
     print("You lost.")
     standings()
     
+def playAgain() :
+    global again
 
+    again = input("\nWould you like to play again? (y/N)\n> ")
+    again = again.upper()
+    again = again[0]
+
+#Initializes Questions:
+def initialize() :
+
+    global computerWins
+    global playerWins
+    global computerChoice
+    global playerChoice
+    global games
+
+    computerWins = 0
+    playerWins = 0
+    computerChoice = 0
+    playerChoice = 0
+    games = 0
+    print("\n-------------- WELCOME TO ROCK PAPER SCISSORS --------------")
+    print("Instructions:\nYou are playing rock paper and scissors against the worlds strongest super computer.\n If you don't beat it, It will take over the world!")
+
+
+    # Simple insurance for minimum required number of games.
+    while games <= 0 :
+        games = int(input("\nHow many matches would you like to play?\n> "))
+        if games <=0 :
+            print("Please input a valid number of games.")
 
 # Introduces the game to the player.
-
-print("\n-------------- WELCOME TO ROCK PAPER SCISSORS --------------")
-print("Instructions:\nYou are playing rock paper and scissors against the worlds strongest super computer.\n If you don't beat it, It will take over the world!")
-
-
-# Simple insurance for minimum required number of games.
-games = 0
-while games <= 0 :
-    games = int(input("\nHow many matches would you like to play?\n> "))
-    if games <=0 :
-        print("Please input a valid number of games.")
-
-computerWins = 0
-playerWins = 0
-computerChoice = 0
-playerChoice = 0
+initialize()
 
 # Main logic loop so that we can tell who wins/loses. 
 # Also tells us which score we need to add to.
 
 active = True
 while active == True:
-    while playerWins < games and computerWins < games :
+
+    while (playerWins < games) and (computerWins < games) :
+        global again
+    
         playerChoice = input("\n\nPlease select rock (R), paper (P) or scissors (S).\n> ")
 
         # Quits application.
         if playerChoice == "quit" :
-            break
+            playAgain()
+            if again == "N" or again == "" :
+                break
+            else :
+                initialize()
+                playerChoice = input("\n\nPlease select rock (R), paper (P) or scissors (S).\n> ")
+
 
         # Refines player and computer choices so they can be evaluated.
         playerChoice = playerChoice.upper()
@@ -137,23 +169,29 @@ while active == True:
     # Concludes the game with a fun message, and asks to play again.
     if playerWins == games :
         print("\n\n\nYou stopped the computer from taking over!\nCongratulations!   (GOOD ENDING)")
+
+        playAgain()
+
+
     elif computerWins == games :
         print("\n\n\nYou let the computer take over... (BAD ENDING)")
-    
-    print("Thanks for playing!")
 
-    again = input("\nWould you like to play again? (y/N)\n> ")
-    again = again.upper()
+        playAgain()
+
+
+    else :
+        break
+
 
     if again == "" :
         active = False
     
+
     elif again[0] == "Y" :
-        computerWins = 0
-        playerWins = 0
-        computerChoice = 0
-        playerChoice = 0
-        continue
+        initialize()
+
 
     else :
         active = False
+
+print("Thanks for playing!")
